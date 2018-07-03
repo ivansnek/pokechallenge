3196bdbb83eec7f0a4ee6c786ebe5f6abe5303f0
Ivan Eusebio <=>
2018-07-03 13:24:42 -0500

    Finish Detaill UI and fix reload with URL path params

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,70 +1,46 @@
 import React from 'react'
-import styled from 'styled-components';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
-const Container = styled.div`
-  width: '100%';
-  height: 80px;
-  background-color: #fff;
-  boder-width: 8px;
-  border-color: #ef5350;
-  cursor:pointer;
-  justify-content: flex-start;
-`;
-
-const Text = styled.span`
-  color: #0060B6;
-  text-decoration: none;
-  color:#00A0C6;
-  text-decoration:none;
-  cursor:pointer;
-  padding: 10px;
-  align-text: center;
-  vertical-align: center;
-  margin-bottom:10px;
-`;
-
 const IMAGE_URL = 'https://img.pokemondb.net/sprites/silver/normal/#.png'
 
 const styles = {
   card: {
-    maxWidth: 200,
+    minWidth: 200,
     margin: 20,
     flexBasis: 200,
-    backgroundColor: '#F9FBE7'
+    backgroundColor: '#F9FBE7',
   },
   media: {
     height: 80,
     margin: 'auto',
     width:80,
   },
-content: {
-  margin: 'auto',
-  textAlign: 'center'
-}
+  content: {
+    margin: 'auto',
+    textAlign: 'center'
+  }
 };
 const PokemonCard = ({ name, onPress, classes }) => {
   return (
-    <Card className={classes.card} onClick={onPress}>
-      <CardMedia
-        className={classes.media}
-        image={IMAGE_URL.replace('#', name)}
-        title={name.toUpperCase()}
-      />
-      <CardContent className={classes.content}>
-        <Link to={`/pokemon/${name}`}>
-          <Typography gutterBottom variant="headline" component="h2">
+    <Link to={`/pokemon/${name}`}>
+      <Card className={classes.card} onClick={onPress}>
+        <CardMedia
+          className={classes.media}
+          image={IMAGE_URL.replace('#', name)}
+          title={name.toUpperCase()}
+        />
+        <CardContent className={classes.content}>
+          <Typography gutterBottom variant="headline" component="h3">
             {name}
           </Typography>
-        </Link>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
 
--- src/pages/PokemonDetail/index.js
+++ src/pages/PokemonDetail/index.js
@@ -1,89 +1,149 @@
 import React from 'react';
 import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Icon from '@material-ui/core/Icon';
+import Card from '@material-ui/core/Card';
 
 import { loadPokemon } from './actions';
+import CONFIG from '../../utils/config';
 
 const Container = styled.div`
   width: '100%';
   height: '100%';
   background-color: white;
-  border-bottom: 2px solid #ddd;
-  width: 50%;
   padding: 25px;
 `;
 
+const EmptyContainer = styled(Container)`
+  &&& {
+    margin: auto;
+    align-items: center;
+  }`;
+
 const RowContainer = styled.div`
-  display:flex;
-  justify-content: flex-start,
-  flex-direction: row
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const IMAGE_URL = 'https://img.pokemondb.net/sprites/silver/normal/#.png'
 
+const styles = {
+  avatar : { width:200, height: 200, borderWidth: 2, borderColor: '#ddd' },
+  text: { margin: 10},
+  cardContainer: { width: '50%', minHeight: 100, margin: 10 }
+};
+
+const DetailContainer = ({style, title, children}) => (
+  <Card className={style}>
+    <List>
+      <ListItem key={"pokemon-title"+title} button={false}>
+        <ListItemIcon>
+          <Icon>star</Icon>
+        </ListItemIcon>
+        <ListItemText inset primary={title} />
+      </ListItem>
+      {children}
+    </List>
+  </Card>
+);
 class PokemonDetail extends React.Component {
   componentDidMount(){
-    this.props.loadPokemon(this.props.url);
+    const { url, loadPokemon, location } = this.props;
+    const pokemon = location.pathname;
+    const loadURL = url ? url : `${CONFIG.API_URL}${pokemon}`
+    loadPokemon(loadURL);
   }
 
-  _renderTypes = () => {
-    return <ul>
-      {this.props.pokemon.types.map((item, index) => <li key={index}> - {item.type.name}</li>)}
-    </ul>
-  }
-
-  _renderStats = () => {
-    return <ul>
-      {this.props.pokemon.stats.map((item, index) =>
-        <li key={index}> - {item.stat.name}: {item.base_stat}</li>
+  _renderTypes = () => (
+    <DetailContainer
+      title="Abilities"
+      style={this.props.classes.cardContainer}
+    >
+     {this.props.pokemon.types.map((item, index) => 
+      <ListItem key={index} button={false}>
+        <ListItemText inset primary={item.type.name} />
+      </ListItem>
       )}
-    </ul>
-  }
-  _renderAbilities = () => {
-    return <ul>
+    </DetailContainer>
+  );
+
+  _renderStats = () => (
+    <DetailContainer
+      title="Abilities"
+      style={this.props.classes.cardContainer}
+    >
+       {this.props.pokemon.stats.map((item, index) =>
+        <ListItem key={index} button={false}>
+          <ListItemText inset primary={`${item.stat.name}: ${item.base_stat}`} />
+        </ListItem>
+      )}
+    </DetailContainer>
+  );
+
+  _renderAbilities = () => (
+    <DetailContainer
+      title="Abilities"
+      style={this.props.classes.cardContainer}
+    >
       {this.props.pokemon.abilities.map((item, index) =>
-        <li key={index}> - {item.ability.name}</li>
+        <ListItem key={index} button={false}>
+          <ListItemText inset primary={item.ability.name} />
+        </ListItem>
       )}
-    </ul>
-  }
+    </DetailContainer>
+  );
 
   render() {
-    const { pokemon } = this.props;
+    const { pokemon, classes } = this.props;
     return (
-      <React.Fragment>
-        <Link to="/" >Go Back</Link>
+      <Container>
+        <Link to="/" >
+          <Button variant="contained" color="primary" >
+          Go Back
+          </Button>
+        </Link>
         {!!pokemon.name ? (
           <React.Fragment>
-            <Container>
-              <RowContainer>
-              <img src={IMAGE_URL.replace('#', pokemon.name)} />
-              <ul>
-                <li><span>Name: {pokemon.name}</span></li>
-                <li><span>Weight: {pokemon.weight}</span></li>
-              </ul>
-              </RowContainer>
-            </Container>
-            <Container>
-              <h2> Type </h2>
-              {this._renderTypes()}
-            </Container>
-            <Container>
-              <h2> Stats </h2>
-              {this._renderStats()}
-            </Container>
-            <Container>
-              <h2> Stats </h2>
+            <RowContainer>
+              <Avatar alt={pokemon.name} src={IMAGE_URL.replace('#', pokemon.name)} className={classes.avatar} />              
+              <Typography gutterBottom variant="headline" component="h1" className={classes.text}>
+                Name: {pokemon.name}
+              </Typography>
+              <Typography gutterBottom variant="headline" component="h2" className={classes.text}>
+                Weight: {pokemon.weight}
+              </Typography>
+            </RowContainer>
+            <RowContainer>
               {this._renderAbilities()}
-            </Container>
+              {this._renderStats()}
+            </RowContainer>
+            {this._renderTypes()}
           </React.Fragment>
-        ) : null
-        }
-      </React.Fragment>
+        ) : (
+        <EmptyContainer>
+          <Typography gutterBottom variant="headline" component="h3">
+            Loading...
+          </Typography>
+        </EmptyContainer>
+      )}
+    </Container>
+        
     );
   }
 }
 
-const mapStateToProps = ({ pokemon }) => ({ pokemon: pokemon.data, url: pokemon.url })
+const mapStateToProps = ({ pokemon }) => ({ pokemon: pokemon.data, url: pokemon.url });
 
-export default connect(mapStateToProps, { loadPokemon })(PokemonDetail);
+const PokemonDetailMaterial = withStyles(styles)(PokemonDetail);
+
+export default connect(mapStateToProps, { loadPokemon })(PokemonDetailMaterial);
