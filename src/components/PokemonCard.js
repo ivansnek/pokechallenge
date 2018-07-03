070fed484bacb485c62f5c9c676e62c8f6f83148
Ivan Eusebio <=>
2018-07-03 12:09:55 -0500

    Pokemon List  with Material UI

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
 const Container = styled.div`
@@ -22,17 +27,45 @@ const Text = styled.span`
   padding: 10px;
   align-text: center;
   vertical-align: center;
+  margin-bottom:10px;
 `;
 
 const IMAGE_URL = 'https://img.pokemondb.net/sprites/silver/normal/#.png'
 
-const PokemonCard = ({ name, onPress }) => {
+const styles = {
+  card: {
+    maxWidth: 200,
+    margin: 20,
+    flexBasis: 200,
+    backgroundColor: '#F9FBE7'
+  },
+  media: {
+    height: 80,
+    margin: 'auto',
+    width:80,
+  },
+content: {
+  margin: 'auto',
+  textAlign: 'center'
+}
+};
+const PokemonCard = ({ name, onPress, classes }) => {
   return (
-    <Container onClick={onPress}>
-      <img key={name} src={IMAGE_URL.replace('#', name)} />
-      <Link to={`/pokemon/${name}`}><Text>{name}</Text></Link>
-    </Container>
+    <Card className={classes.card} onClick={onPress}>
+      <CardMedia
+        className={classes.media}
+        image={IMAGE_URL.replace('#', name)}
+        title={name.toUpperCase()}
+      />
+      <CardContent className={classes.content}>
+        <Link to={`/pokemon/${name}`}>
+          <Typography gutterBottom variant="headline" component="h2">
+            {name}
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
   );
 }
 
-export default PokemonCard;
+export default withStyles(styles)(PokemonCard);
